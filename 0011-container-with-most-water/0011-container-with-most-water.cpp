6f6d9ff6a29368c0d311abe06ba55be8c3ba5499
Nishant Bujethiya <bujethiyanishant@gmail.com>
2026-07-10 03:03:32 +0530

    Time: 0 ms (100%), Space: 62.8 MB (78.77%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,27 +1,54 @@
+// my solution 
+
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         vector<int> area;
+//         int n = height.size();
+//         int i=0;
+//         int j=n-1;
+//         while(i<j){
+//             int comp = min(height[i],height[j]);
+//             area.push_back(comp*(j-i));
+//             if(height[i]<height[j]){
+//                 i++;
+//             }else if(height[i]>height[j]){
+//                 j--;
+//             }else{
+//                 i++;
+//             }
+//         }
+//         int maxArea = area[0];
+//         for (int i = 1; i < area.size(); i++) {
+//             if (area[i] > maxArea) {
+//                 maxArea = area[i];
+//             }
+//         }
+//     return maxArea;  
+//     }
+// };
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        vector<int> area;
         int n = height.size();
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            int comp = min(height[i],height[j]);
-            area.push_back(comp*(j-i));
-            if(height[i]<height[j]){
-                i++;
-            }else if(height[i]>height[j]){
+        int i = 0;
+        int j = n - 1;
+        int maxWater = 0;
+        while (i < j) {
+            int w = j - i;
+            int h = min(height[i], height[j]);
+            int area = w * h;
+            maxWater = max(maxWater, area);
+            if (height[i] > height[j]) {
                 j--;
-            }else{
+            }
+            else {
                 i++;
             }
         }
-        int maxArea = area[0];
-        for (int i = 1; i < area.size(); i++) {
-            if (area[i] > maxArea) {
-                maxArea = area[i];
-            }
-        }
-    return maxArea;  
+        return maxWater;
     }
 };
+
+//expected one
