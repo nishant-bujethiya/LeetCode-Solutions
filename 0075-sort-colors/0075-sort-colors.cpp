d0ca422e9348ed6f01c3402c2a6aac228b521f73
Nishant Bujethiya <bujethiyanishant@gmail.com>
2026-07-11 14:36:20 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,38 +1,67 @@
+// my approach : first sort all the zeroes then ones;
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n = nums.size();
+//         int i = 0;
+//         int j = n - 1;
+//         int count = 0;
+//         while(i <= j){
+//             if(nums[i] == 0){
+//                 count++;
+//                 i++;
+//             }
+//             else if(nums[j] == 0){
+//                 swap(nums[i], nums[j]);
+//                 count++;
+//                 i++;
+//                 j--;
+//             }
+//             else{
+//                 j--;
+//             }
+//         }
+//         i = count;
+//         j = n - 1;
+//         while(i <= j){
+//             if(nums[i] == 1){
+//                 i++;
+//             }
+//             else if(nums[j] == 1){
+//                 swap(nums[i], nums[j]);
+//                 i++;
+//                 j--;
+//             }
+//             else{
+//                 j--;
+//             }
+//         }
+//     }
+// };
+
+// using dutch national flag algorithm 
+// using 3 pointers method ; low to sort zeroes;mid to sort ones; high to sort twos;
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int i = 0;
-        int j = n - 1;
-        int count = 0;
-        while(i <= j){
-            if(nums[i] == 0){
-                count++;
-                i++;
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
             }
-            else if(nums[j] == 0){
-                swap(nums[i], nums[j]);
-                count++;
-                i++;
-                j--;
+            else if(nums[mid] == 1) {
+                mid++;
             }
-            else{
-                j--;
-            }
-        }
-        i = count;
-        j = n - 1;
-        while(i <= j){
-            if(nums[i] == 1){
-                i++;
-            }
-            else if(nums[j] == 1){
-                swap(nums[i], nums[j]);
-                i++;
-                j--;
-            }
-            else{
-                j--;
+            else {   
+                swap(nums[mid], nums[high]);
+                high--;
             }
         }
     }
