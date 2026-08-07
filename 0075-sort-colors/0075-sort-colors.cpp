aa5c1f00893f658e29be5b21e854c6665f17890e
Nishant Bujethiya <bujethiyanishant@gmail.com>
2026-08-07 19:52:28 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.54%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -66,3 +66,33 @@ public:
         }
     }
 };
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int zero = 0;
+//         int one = 0;
+//         int two = 0;
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] == 0)
+//                 zero++;
+//             else if (nums[i] == 1)
+//                 one++;
+//             else
+//                 two++;
+//         }
+//         int index = 0;
+//         while (zero--) {
+//             nums[index] = 0;
+//             index++;
+//         }
+//         while (one--) {
+//             nums[index] = 1;
+//             index++;
+//         }
+//         while (two--) {
+//             nums[index] = 2;
+//             index++;
+//         }
+//     }
+// };
